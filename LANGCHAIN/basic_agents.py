f18f83e7e9a1b2abaf5fdae93653e20744e1719a
Manoj Athreya A <39020374+ManojAthreya@users.noreply.github.com>
2024-04-22 22:22:04 -0400

    learning agents

--- LANGCHAIN/basic_agents.py
+++ LANGCHAIN/basic_agents.py
@@ -10,6 +10,8 @@ from operator import itemgetter
 from typing import Dict, List, Union
 from langchain_core.messages import AIMessage
 from langchain_core.runnables import Runnable
+from langchain import hub
+from langchain.agents import AgentExecutor, create_openai_tools_agent
 
 warnings.simplefilter("ignore")
 load_dotenv()
@@ -20,6 +22,7 @@ os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
+prompt = hub.pull("hwchase17/openai-tools-agent")
 
 @tool
 def multiply(int1:int, int2:int) -> int:
@@ -41,22 +44,31 @@ llm = ChatGroq(model_name='llama3-70b-8192', temperature=0)
 
 
 tools = [multiply, exponentiate, add]
-llm_with_tools = llm.bind_tools(tools)
-tool_map = {tool.name: tool for tool in tools}
+# llm_with_tools = llm.bind_tools(tools)
+# tool_map = {tool.name: tool for tool in tools}
 
 
-def call_tools(msg: AIMessage) -> Runnable:
-    """Simple sequential tool calling helper."""
-    tool_map = {tool.name: tool for tool in tools}
-    tool_calls = msg.tool_calls.copy()
-    for tool_call in tool_calls:
-        tool_call["output"] = tool_map[tool_call["name"]].invoke(tool_call["args"])
-    return tool_calls
+# def call_tools(msg: AIMessage) -> Runnable:
+#     """Simple sequential tool calling helper."""
+#     tool_map = {tool.name: tool for tool in tools}
+#     tool_calls = msg.tool_calls.copy()
+#     for tool_call in tool_calls:
+#         tool_call["output"] = tool_map[tool_call["name"]].invoke(tool_call["args"])
+#     return tool_calls
 
 
-chain = llm_with_tools | call_tools
+# chain = llm_with_tools | call_tools
 
-print(chain.invoke("cube thirty-seven"))
+# print(chain.invoke("cube thirty-seven"))
+
+agent = create_openai_tools_agent(llm, tools, prompt)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 
+agent_executor.invoke(
+    {
+        "input": "Take 3 to the fifth power and multiply that by the sum of twelve and three, then square the whole result"
+    }
+)
 
